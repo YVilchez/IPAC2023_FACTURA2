dced6dd2d0b31933c31ce4be70d89e9a214c35bc
Nahum Casco <nahumcasco@gmail.com>
2023-02-13 20:58:47 -0600

    Login
    Login

---
+++ Facturacion/Entidades/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Login
+    {
+        public string CodigoUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Rol { get; set; }
+        public DateTime MyProperty { get; set; }
+
+        public Login()
+        {
+        }
+
+        public Login(string codigoUsuario, string contraseña, string rol)
+        {
+            CodigoUsuario=codigoUsuario;
+            Contraseña=contraseña;
+            Rol=rol;
+        }
+    }
+}
---
+++ Facturacion/Vista/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            if (UsuarioTextBox.Text == string.Empty)
+            {
+                errorProvider1.SetError(UsuarioTextBox, "Ingrese un usuario");
+                UsuarioTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+            if (string.IsNullOrEmpty(ContraseñaTextBox.Text))
+            {
+                errorProvider1.SetError(ContraseñaTextBox, "Ingrese una contraseña");
+                ContraseñaTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+
+
+        }
+    }
+}
